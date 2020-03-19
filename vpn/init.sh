e024375926be055b41122439cca32a99af6f3596
poma <semenov.roma@gmail.com>
2020-03-19 10:04:02 +0300

    vpn-script

--- vpn/init.sh
+++ vpn/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
+# usage: init.sh 1.2.3.4
 # more info: https://github.com/kylemanna/docker-openvpn/blob/master/docs/docker-compose.md
-docker-compose run --rm openvpn ovpn_genconfig -u udp://vpn.viking-studio.com
+HOST=${1:-$(echo $DOCKER_HOST | awk -F'[/:]' '{print $4}')} # first arg or docker host from env var
+echo "Initializing config for host '$HOST'"
+docker-compose run --rm openvpn ovpn_genconfig -u udp://$HOST
 docker-compose run --rm openvpn ovpn_initpki
 docker-compose run --rm openvpn easyrsa build-client-full poma nopass
 docker-compose run --rm openvpn ovpn_getclient poma > poma.ovpn
