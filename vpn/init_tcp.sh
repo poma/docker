a130be2b4b2a39d6aa703ebc6a31d1e8ef6b9273
poma <semenov.roma@gmail.com>
2020-04-15 07:43:20 +0300

    vpn updates

--- vpn/init_tcp.sh
+++ vpn/init_tcp.sh
@@ -3,8 +3,8 @@
 # more info: https://github.com/kylemanna/docker-openvpn/blob/master/docs/docker-compose.md
 HOST=${1:-$(echo $DOCKER_HOST | awk -F'[/:]' '{print $4}')} # first arg or docker host from env var
 echo "Initializing config for host '$HOST'"
-docker-compose run --rm openvpn ovpn_genconfig -u tcp://$HOST:443
-docker-compose run --rm openvpn bash -c 'yes | ovpn_initpki nopass'
-docker-compose run --rm openvpn easyrsa build-client-full poma nopass
-docker-compose run --rm openvpn ovpn_getclient poma > poma.ovpn
-docker-compose up -d
+docker-compose -f docker-compose.tcp.yml run --rm openvpn ovpn_genconfig -u tcp://$HOST:443
+docker-compose -f docker-compose.tcp.yml run --rm openvpn bash -c 'yes | ovpn_initpki nopass'
+docker-compose -f docker-compose.tcp.yml run --rm openvpn easyrsa build-client-full poma nopass
+docker-compose -f docker-compose.tcp.yml run --rm openvpn ovpn_getclient poma > poma.ovpn
+docker-compose -f docker-compose.tcp.yml up -d
