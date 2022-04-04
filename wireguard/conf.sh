557a3db1873fcdf8c6ed6ae0569878ea811ceef6
poma <semenov.roma@gmail.com>
2022-04-04 10:49:20 +0400

    wireguard conf download fix

--- wireguard/conf.sh
+++ wireguard/conf.sh
@@ -3,4 +3,4 @@ ID=${1:-1}
 if ! [[ $ID =~ '^[0-9]+$' ]] ; then
    ID="_$ID"
 fi
-docker-compose cp wireguard:/config/peer$ID/peer$ID.conf ./wg$ID.conf
+docker compose cp wireguard:/config/peer$ID/peer$ID.conf ./wg$ID.conf
